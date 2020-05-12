4a0f51c78d7e435fb5e55b251d37765f71cbb4c6
Brad Baker <bwb0013@uah.edu>
2020-05-12 16:24:01 -0500

    added parse arguments for user input

--- download.py
+++ download.py
@@ -6,10 +6,35 @@ This script allows for filtering the download csv by a desired scene or asset ty
 The expected columns are scene_id, asset_type, and links.
 """
 
+import argparse
 import os
 import pandas as pd
 import re
 import requests
+
+
+def parse_arguments():
+    """
+        python download.py input.csv --filtercolumn asset_type --filtervalue udm --downloadfolder downloads
+    """
+
+    parser = argparse.ArgumentParser(
+        description='This script allows for filtering the download csv by a desired scene or asset type. URL links for the selected data downloads are returned. The expected columns are scene_id, asset_type, and links.')
+    parser.add_argument('inputcsv', type=str,
+                        help='path to the input csv file')
+    parser.add_argument('--filtercolumn', type=str,
+                        help='column that you want to filter on.', choices=['scene_id', 'asset_type'])
+    parser.add_argument('--filtervalue', type=str,
+                        help='value to filter by')
+    parser.add_argument('--downloadfolder', type=str, default="downloads",
+                        help='name of the folder to download to')
+
+    args = parser.parse_args()
+    # Check either both or neither of filtercolumn and filtervalue have been passed
+    if len([x for x in (args.filtercolumn, args.filtervalue) if x is not None]) == 1:
+        parser.error('--filtercolumn and --filtervalue must be given together')
+
+    return args.inputcsv, args.filtercolumn, args.filtervalue, args.downloadfolder
 
 
 def filter_by(key, value, df):
@@ -78,8 +103,12 @@ def ingest_csv(csv_file_name, filter_dict):
     """
 
     df = pd.read_csv(csv_file_name)
-    df = filter_by(df=df, key=filter_dict['key'], value=filter_dict['value'])
-
+    if len(df) == 0:
+        print("The csv file was empty.")
+    if filter_dict['key'] and filter_dict['value']:
+        df = filter_by(df=df, key=filter_dict['key'], value=filter_dict['value'])
+        if len(df) == 0:
+            print("The filter returned zero rows.")
     return df
 
 
@@ -104,6 +133,7 @@ def main(csv_file_name, download_folder_name, filter_dict):
     create_folder_if_not_exist(download_folder_name)
 
     df = ingest_csv(csv_file_name, filter_dict)
+
     create_additional_columns(df)
 
     #    download files from csv one row at a time
@@ -113,4 +143,5 @@ def main(csv_file_name, download_folder_name, filter_dict):
 
 
 if __name__ == '__main__':
-    main("leancsv.csv", "downloads", {'key': 'asset_type', 'value': 'udm'})
+    csv_file_name, filter_column, filter_value, download_folder_name = parse_arguments()
+    main(csv_file_name, download_folder_name, {'key': filter_column, 'value': filter_value})
