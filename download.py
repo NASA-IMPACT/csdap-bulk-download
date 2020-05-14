872cf5a801dfa8860d4eb32b096a3902260f50a5
Brad Baker <bwb0013@uah.edu>
2020-05-14 15:38:04 -0500

    add parameters and return values to doc strings

--- download.py
+++ download.py
@@ -25,6 +25,11 @@ EXPECTED_COLUMNS = ("scene_id", "asset_type", "link")
 def parse_arguments():
     """
         python download.py input.csv --filtercolumn asset_type --filtervalue udm --downloadfolder downloads
+            Parameters:
+                None
+            Returns:
+                tuple of arguments: args.inputcsv, args.filtercolumn, args.filtervalue, args.downloadfolder
+
     """
 
     parser = argparse.ArgumentParser(
@@ -49,6 +54,12 @@ def parse_arguments():
 def filter_by(key, value, df):
     """
         Function to allow user to filter csv for desired data
+            Parameters:
+                key (str): column name to filter by
+                value (str): value to filter by
+                df (DataFrame): input dataframe 
+            Returns:
+                df (DataFrame): filtered dataframe
     """
 
     return df[df[key] == value]
@@ -57,6 +68,10 @@ def filter_by(key, value, df):
 def file_exists(path):
     """
         Function to see if file has already been downloaded
+            Parameters:
+                path (str): path to check if the folder exists 
+            Returns:
+                bool: does the path exist
     """
 
     return os.path.exists(path)
@@ -65,6 +80,10 @@ def file_exists(path):
 def create_folder_if_not_exist(folder_name):
     """
         Function to create a new folder to download links to ()
+            Parameters:
+                folder_name (str): name of the folder to create
+            Returns:
+                None
     """
 
     if not os.path.exists(folder_name):
@@ -74,6 +93,12 @@ def create_folder_if_not_exist(folder_name):
 def download(link, file_name, folder_name):
     """
         Function to download link to desired data
+            Parameters:
+                link (str): link to file being downloaded
+                file_name (str): filename for the downloaded file              
+                folder_name (str): folder to put the file in
+            Returns:
+                None
     """
 
     path = f"{folder_name}/{file_name}"
@@ -91,6 +116,10 @@ def download(link, file_name, folder_name):
 def extract_file_name(row):
     """
         Function to extract file name from S3 link.
+            Parameters:
+                row (DataFrame row): one row from the csv file
+            Returns:
+                str: filename of the file to be downloaded
     """
 
     return re.search('(PS.*)\?', row["link"]).group(0)[:-1]
@@ -99,15 +128,26 @@ def extract_file_name(row):
 def download_row(row, download_folder_name):
     """
         Function to download file from row in csv.
+            Parameters:
+                row (DataFrame row): one row from the csv file
+                download_folder_name (str): name of the folder to download the file to
+            Returns:
+                None
     """
 
-    download(link="http://google.com/favicon.ico",
+    download(link="ht://google.com/favicon.ico",
              file_name=row['file_name'], folder_name=os.path.join(download_folder_name, row['folder_name']))
 
 
 def ingest_csv(csv_file_name, filter_dict):
     """
         Function to ingest csv file and filter it.
+            Parameters:
+                csv_file_name (str): csv file to ingest
+                filter_dict (dict): dictionary that holds filter parameters. Example {'key': filter_column, 'value': filter_value}
+            Returns:
+                df (DataFrame): dataframe for the ingested csv
+                empty (bool): is the dataframe empty
     """
 
     # read the csv file
@@ -138,6 +178,10 @@ def ingest_csv(csv_file_name, filter_dict):
 def create_additional_columns(df):
     """
         Function to create additional columns for download.
+            Parameters:
+                df (DataFrame): input dataframe
+            Returns:
+                df (DataFrame): dataframe with additional columns
     """
 
     df["scene_type"] = df.scene_id.str.split("-").str[0]
@@ -153,6 +197,12 @@ def create_additional_columns(df):
 def main(csv_file_name, download_folder_name, filter_dict):
     """
         Main function that drives the code.
+            Parameters:
+                csv_file_name (str): name of the ingested csv
+                download_folder_name (str): folder to download all of the files to
+                filter_dict (dict): dictionary that holds filter parameters. Example {'key': filter_column, 'value': filter_value}
+            Returns:
+                None
     """
 
     create_folder_if_not_exist(download_folder_name)
