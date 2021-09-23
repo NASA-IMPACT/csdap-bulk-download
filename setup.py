fa0bdac17a6233c94e5f043af9f007e2af03e1bb
Anthony Lukach <anthonylukach@gmail.com>
2021-09-23 16:09:21 -0600

    Rework packages

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ import setuptools
 setuptools.setup(
     name="csdap_bulk_download",
     version="1.0",
+    description="Bulk download tool for CSDAP orders.",
     author="NASA Impact",
     author_email="csdap@uah.edu",
-    description="Bulk download tool for CSDAP orders.",
+    maintainer="Anthony Lukach",
+    maintainer_email="anthony@developmentseed.org",
+    url="https://github.com/nasa-impact/csdap-bulk-download",
     install_requires=[
         "Click",
         "requests >= 2.24.0",
     ],
-    packages=setuptools.find_packages(),
+    packages=["csdap_bulk_download"],
     entry_points={
         "console_scripts": [
             "csdap-bulk-download = csdap_bulk_download.cli:cli",
