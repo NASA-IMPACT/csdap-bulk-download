74eb890255d6492a3fc62d251649dc3498f2e7de
meghana-paramesh <mp0157@uah.edu>
2022-02-10 10:51:16 -0600

    Resolved the issues with black linting

--- csdap_bulk_download/csdap.py
+++ csdap_bulk_download/csdap.py
@@ -67,19 +67,23 @@ class CsdapClient:
                 f"got {response.status_code}"
             )
 
-        querystring = parse_qs(urlparse(response.headers["Location"]).query) 
-        if querystring.get("error") and response.status_code == 302 and 'resolution_url' in response.text:
-            start = response.text.find('resolution_url')+len('resolution_url')+1
-            end = response.text.find('\"', start)
+        querystring = parse_qs(urlparse(response.headers["Location"]).query)
+        if (
+            querystring.get("error")
+            and response.status_code == 302
+            and "resolution_url" in response.text
+        ):
+            start = response.text.find("resolution_url") + len("resolution_url") + 1
+            end = response.text.find('"', start)
             raise AuthError(
                 "\n".join(
                     [
-                       "Authorization required for this application, please authorize by visiting the resolution url",
-                       response.text[start:end],
+                        "Authorization required for this application, please authorize by visiting the resolution url",
+                        response.text[start:end],
                     ]
                 )
             )
-            
+
         if querystring.get("error"):
             raise AuthError(querystring["error_msg"])
 
