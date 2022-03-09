e734352b299a8b86045d6f2169ff4e907f0bb3dc
vincentsarago <vincent.sarago@gmail.com>
2022-03-09 09:23:38 +0100

    add module version in python code

---
+++ csdap_bulk_download/__init__.py
@@ -0,0 +1,3 @@
+"""csdap_bulk_download."""
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 
 setuptools.setup(
     name="csdap_bulk_download",
-    version="1.0",
     description="Bulk download tool for CSDAP orders.",
     author="NASA Impact",
     author_email="csdap@uah.edu",
